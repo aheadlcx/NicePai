1337a949bb3f710f7855074bdddf996a54ae289f
aheadlcx <aheadlcxzhang@gmail.com>
2016-05-21 12:20:16 +0800

    delete data module file

--- data/build/intermediates/classes/androidTest/debug/me/aheadlcx/nicepai/data/BR.java
+++
@@ -1,5 +0,0 @@
-package me.aheadlcx.nicepai.data;
-
-public class BR {
-        public static final int _all = 0;
-}
--- data/build/intermediates/classes/androidTest/debug/me/aheadlcx/nicepai/data/test/BR.java
+++
@@ -1,5 +0,0 @@
-package me.aheadlcx.nicepai.data.test;
-
-public class BR {
-        public static final int _all = 0;
-}
