f6439b5fb53224e687b1b1d1ebf89c8235d453b0
aheadlcx <aheadlcxzhang@gmail.com>
2017-03-21 19:50:17 +0800

    before reconstruction

---
+++ app/src/main/java/me/aheadlcx/nicepai/test/TestRxJava.java
@@ -0,0 +1,33 @@
+package me.aheadlcx.nicepai.test;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import me.aheadlcx.nicepai.base.BaseActivity;
+import rx.Observable;
+import rx.Subscriber;
+
+/**
+ * Description:
+ * Creator: aheadlcx
+ * Date:11/11/2016 5:07 PM
+ */
+
+public class TestRxJava extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        Subscriber
+//        Observable.OnSubscribe
+        Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("Ss");
+                subscriber.onCompleted();
+            }
+        });
+
+//        Observable.just()
+    }
+}
